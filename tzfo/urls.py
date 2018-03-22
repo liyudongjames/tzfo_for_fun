ea17dc45b67efb2d7ce4393fd40a997c87f61da5
liyudongjames <liyudongjames@sina.com>
2018-03-22 16:50:13 +0800

    change add2 add into new_add

--- tzfo/urls.py
+++ tzfo/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('', three_view.index),
     path('add/', three_view.add_test),
     path('add/<int:a>/<int:b>/', three_view.add_test_two, name='add'),
-    path('add/<int:a>/<int:b>/', three_view.add_test_two, name='add2'),
+    path('new_add/<int:a>/<int:b>/', three_view.add_test_two, name='add2'),
 ]
