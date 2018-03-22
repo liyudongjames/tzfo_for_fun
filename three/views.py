f58f94150d52241298a0f1c2e127f216ff2146d1
liyudongjames <liyudongjames@sina.com>
2018-03-22 10:57:10 +0800

    test a html

--- three/views.py
+++ three/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse(u'欢迎光临11111')
+    return render(request, 'home.html')
 
 
 def add_test(request):
